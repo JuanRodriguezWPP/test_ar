21cbcc3b25752196a631751624b355b64c48d461
juan.rodriguezv@wppmedia.com <juan.rodriguezv@wppmedia.com>
2026-08-10 14:37:53 -0500

    test altar

--- src/portal.js
+++ src/portal.js
@@ -308,7 +308,7 @@ function buildInterior() {
 
   // Cargar imagen panorámica equirectangular
   const tex = new THREE.TextureLoader().load(
-    '/models/360img2.png',
+    '/models/360img.png',
     () => { /* cargada OK */ },
     undefined,
     (e) => console.warn('Error cargando panorama 360:', e)
